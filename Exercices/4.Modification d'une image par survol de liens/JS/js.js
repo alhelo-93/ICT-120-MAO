a832890671371ec61c71dafaf4edd270680266ef
ALHELO Marwan <Marwan.ALHELO@cpnv.ch>
2019-10-31 16:55:18 +0100

    4 exo

---
+++ Exercices/4.Modification d'une image par survol de liens/JS/js.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    a1.addEventListener("mouseover", changphoto1);
+    a1.addEventListener("mouseout", changphoto1);
+    a2.addEventListener("mouseover", changphoto2);
+    a3.addEventListener("mouseover", changphoto3);
+}
+
+function changphoto1() {
+
+        img.src="PHOTOS/airplane.png";
+
+}
+function changphoto2() {
+
+
+    img.src="PHOTOS/Bike.png";
+
+}
+function changphoto3() {
+
+
+    img.src="PHOTOS/ElectricCar.png";
+
+}
+function changphoto4() {
+
+
+    img.src="PHOTOS/Bike.png";
+
+}
